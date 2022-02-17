377bdd587b4a9b0cdd5ab1575297c181cf12c03d
GuiJordao21 <gui.jordao.1994@gmail.com>
2022-02-17 07:41:15 -0300

    api adapter

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,8 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/GuiJordao21/hex-arch/internal/adapters/core/arithmetic"
-)
-
 func main() {
-	arithAdapter := arithmetic.NewAdapter()
-	result, err := arithAdapter.Addition(3, 1)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(result)
+
+	//Ports
+	// var core ports.ArithmeticPort
+
 }
